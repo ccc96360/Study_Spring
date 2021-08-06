9b35f69f7cb38abf8161fee3ca1cb14581151bd7
ccc96360 <ccc96360@naver.com>
2021-08-06 16:26:26 +0900

    양속선 전이(Cascade)와 고아 객체(rphanRemoval)

--- jpabasic/ex1-hellojpa/src/main/java/hellojpa/JpaMain.java
+++ jpabasic/ex1-hellojpa/src/main/java/hellojpa/JpaMain.java
@@ -15,27 +15,20 @@ public class JpaMain {
 
         tx.begin();
         try {
+            Child child1 = new Child();
+            Child child2 = new Child();
 
-            Team team = new Team();
-            team.setName("teamA");
-            em.persist(team);
+            Parent parent = new Parent();
+            parent.addChild(child1);
+            parent.addChild(child2);
 
-            Member member = new Member();
-            member.setName("Hello");
-            member.setTeam(team);
-
-            em.persist(member);
+            em.persist(parent);
 
             em.flush();
             em.clear();
 
-            Member m = em.find(Member.class, member.getId());
-
-            System.out.println("name = " + m.getName());
-
-            System.out.println("==================================");
-            System.out.println("m = " + m.getTeam().getClass());
-            System.out.println("==================================");
+            Parent findParent = em.find(Parent.class, parent.getId());
+            findParent.getChildList().remove(0);
 
             tx.commit();
         } catch (Exception e){
