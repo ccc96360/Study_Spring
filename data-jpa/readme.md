87b045fb732ed0013f9880bb85ea0481f0d23a72
ccc96360 <ccc96360@naver.com>
2021-09-23 17:33:15 +0900

    쿼리 메소드 - @Query에 직접 JPQL 작성하기

--- data-jpa/readme.md
+++ data-jpa/readme.md
@@ -31,3 +31,24 @@ public interface 엔티티repository extends JpaRepository~~~{
 ```
 - ```엔티티명.메서드명```과 repository의 ```메서드명```을 일치시키면 @Query 가 없어도 동작한다.
 - 우선순위 : NamedQuery에 일치하는 메서드명 확인 -> 메서드 이름으로 쿼리 생성
+
+## 2. @Query에 직접 JPQL 작성하기
+### 2.1 리포지토리 메소드에 쿼리 정의
+```java
+    @Query("select m from Member m where m.userName = :userName and m.age = :age")
+    List<Member> findUser(@Param("userName") String userName, @Param("age") int age);
+```
+- 위와 같이 @Query안에 바로 JPQL 을 작성할 수 있다.
+- 쿼리에 오타 존재시 애플리케이션 로딩 시점에서 에러가 발생한다.
+
+### 2.2 값이나 DTO로 조회
+- 단순한 값 조회
+```java
+    @Query("select m.userName from Member m")
+    List<String> findUserNameList();
+```
+- DTO로 조회
+```java
+@Query("select new 패키지명.DTO(m.id, m.userName, t.name) from Member m join m.team t")
+    List<DTO> findDto();
+```
--- data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
+++ data-jpa/src/test/java/study/datajpa/repository/MemberRepositoryTest.java
@@ -7,7 +7,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.dto.MemberDto;
 import study.datajpa.entity.Member;
+import study.datajpa.entity.Team;
 
 import java.util.List;
 
@@ -19,6 +21,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class MemberRepositoryTest {
 
     @Autowired MemberRepository memberRepository;
+    @Autowired TeamRepository teamRepository;
 
     @BeforeEach
     public void cleanUp(){
@@ -102,5 +105,49 @@ public class MemberRepositoryTest {
         Member member = res.get(0);
 
         assertThat(member).isEqualTo(m2);
+    }
+
+    @Test
+    public void testQuery(){
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<Member> res = memberRepository.findUser("AAA", 10);
+        Member findMember = res.get(0);
+
+        assertThat(findMember).isEqualTo(m1);
+    }
+
+    @Test
+    public void findUserNameList(){
+        Member m1 = new Member("AAA", 10);
+        Member m2 = new Member("BBB", 20);
+
+        memberRepository.save(m1);
+        memberRepository.save(m2);
+
+        List<String> userNameList = memberRepository.findUserNameList();
+        for (String s : userNameList) {
+            System.out.println("s = " + s);
+        }
+    }
+
+    @Test
+    public void findMemberDto(){
+        Team team = new Team("teamA");
+        teamRepository.save(team);
+
+        Member m1 = new Member("AAA", 10);
+        m1.setTeam(team);
+        memberRepository.save(m1);
+
+
+        List<MemberDto> dtos = memberRepository.findMemberDto();
+        for (MemberDto dto : dtos) {
+            System.out.println("dto = " + dto);
+        }
     }
 }
